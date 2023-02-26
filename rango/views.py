0f575df9bb271f1bbd402194d54d33097503d710
Mark Leniw <mleniw@protonmail.com>
2023-02-26 07:31:14 -0800

    Third commit

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+
+def about(request):
+    return HttpResponse("Rango says this is the About page.")
